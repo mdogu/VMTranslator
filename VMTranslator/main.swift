34d24d97df37af6cdb7e62653bb351fbba362e4c
Murat Doğu <muratdogu@murats-macbook-pro.local>
2017-12-26 13:34:46 +0300

    Initial Commit

---
+++ VMTranslator/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  VMTranslator
+//
+//  Created by Murat Dogu on 26.12.2017.
+//  Copyright © 2017 Murat Dogu. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
